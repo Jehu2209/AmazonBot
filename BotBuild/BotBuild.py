7aebff1b9426956968f862f1f564864b482b4a77
Jehu2209 <48810434+Jehu2209@users.noreply.github.com>
2022-01-02 18:57:20 -0800

    Update BotBuild.py

--- BotBuild/BotBuild.py
+++ BotBuild/BotBuild.py
@@ -1,1 +1,12 @@
 # code go here
+# A simplified file which takes the old parts of bot build and stream lines them.
+
+# Imports
+import json
+import os
+
+# PART 1 JSON READING
+data = f'{os.getcwd()}'.split('\\')
+
+print(data)
+ 
